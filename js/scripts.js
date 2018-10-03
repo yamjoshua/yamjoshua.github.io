757a44bf0407a231ac728a9bce96e596fe521a59
Joshua Yam <jy2223@nyu.edu>
2018-10-03 16:16:47 -0400

    Blur effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 	$("#trigger").click(function() {
 	   $('html, body').animate({scrollTop:$(window).height()}, 1000);
-    return false;
 	});
+	$(window).scroll(function() {
+    var s = 2.0*($(window).scrollTop()),
+    opacityVal = (s / 1000.0);
+    $('.blur').css('opacity', opacityVal);
+});
 });
