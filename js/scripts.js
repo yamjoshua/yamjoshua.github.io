511987cc282f712929542f02082e5ab195bd7921
Joshua Yam <jy2223@nyu.edu>
2018-10-04 04:09:06 -0400

    New Links

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	   $('html, body').animate({scrollTop:$(window).height()}, 1000);
 	});
 	$(window).scroll(function() {
-    var s = 2.0*($(window).scrollTop()),
+    var s = 2.0*($(window).scrollTop())-400,
     opacityVal = (s / 1000.0);
     $('.blur').css('opacity', opacityVal);
-});
+	});
 });
