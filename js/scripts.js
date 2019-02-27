d748feb5f738c2f83a46d8562b229bf3d7ad1b4d
Joshua Yam <jy2223@nyu.edu>
2019-02-27 15:51:04 -0500

    Autoscroll

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 	$("#trigger").click(function() {
 	   $('html, body').animate({scrollTop:$(window).height()}, 1000);
 	});
+
 	$(window).scroll(function() {
     var s = 2.0*($(window).scrollTop())-400,
     opacityVal = (s / 1000.0);
     $('.blur').css('opacity', opacityVal);
 	});
+
+   	$('html, body').delay(7000).animate({scrollTop:$(window).height()}, 1000);
+
 });
